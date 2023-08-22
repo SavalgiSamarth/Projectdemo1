4308da74460574128028b518a2469a36c41c53af
SAVALGI <SAVALGI@LAPTOP-86321MPS>
2023-08-22 23:40:40 +0530

    uploading projectdemo1

---
+++ DemoProject1/src/Git/Projectdemo1.java
@@ -0,0 +1,9 @@
+package Git;
+public class Projectdemo1 {
+	public static void main (String[]args) {
+		int a;
+		a=10;
+		
+	}
+
+}
---
+++ Sammy/selenium/car.java
@@ -0,0 +1,8 @@
+package selenium;
+
+public class car {
+public static void main (String[]args) {
+	int a=10;
+System.out.println(a);
+}
+}
